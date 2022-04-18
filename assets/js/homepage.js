ac2e591f484adb4d584dfd5b59cfb2d8e8759050
crizano <69006108+crizano@users.noreply.github.com>
2022-04-18 18:30:00 -0500

    updated javascript and added API call

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,11 +1,13 @@
-var getUserRepos = function () {
-    fetch("https://api.github.com/users/octocat/repos").then(function(response) {
-        response.json().then(function(data) {
-            console.log(data);
-    });
-});
+var getUserRepos = function (user) {
+    // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
-console.log("outside");
+  // make a request to the url
+  fetch(apiUrl).then(function(response) {
+    response.json().then(function(data) {
+      console.log(data);
+    });
+  });
 };
 
-getUserRepos();
+getUserRepos("shane-sepeda");
